376ae77c7a930f64dda5a14f00aebc0113fe6358
Raynos <raynos2@gmail.com>
2014-12-17 13:17:13 -0800

    rewrite tests with mock kafka instead of nodesol

--- test/kafka.js
+++ test/kafka.js
@@ -1,65 +1,37 @@
 var test = require('tape');
-var uuid = require('uuid');
 var test = require('tape');
-var net = require('net');
-var NodeSol = require('uber-nodesol').NodeSol;
+var KafkaServer = require(
+    'kafka-logger/test/lib/kafka-server.js');
 
 var Logger = require('../logger.js');
 var KafkaBackend = require('../backends/kafka.js');
 
 test('kafka logging', function (assert) {
-    var socket = net.connect({ host: 'localhost', port: 2181 });
+    var server = KafkaServer(function onMessage(msg) {
+        assert.equal(msg.topic, 'rt-foobar');
 
-    socket.once('connect', function () {
-        socket.destroy();
+        var obj = msg.messages[0].payload;
 
-        var logger = Logger({
-            meta: {
-                team: 'rt',
-                project: 'foobar'
-            },
-            backends: {
-                kafka: KafkaBackend({
-                    host: 'localhost',
-                    port: 2181
-                })
-            }
-        });
+        assert.equal(obj.level, 'info');
+        assert.ok(obj.msg.indexOf('writing to kafka') !== -1);
 
-        var client = new NodeSol({ host: 'localhost', port: 2181 });
-        client.connect(function (err) {
-            /*jshint camelcase: false*/
-            assert.ifError(err);
-
-            var proc = client.get_producer('rt-foobar');
-
-            client.create_consumer(uuid(), 'rt-foobar', {}, function (stream) {
-                if (!stream) {
-                    assert.ok(false, 'no stream');
-                    return assert.end();
-                }
-
-                stream.once('data', function (chunk) {
-                    var obj = JSON.parse(String(chunk));
-
-                    assert.equal(obj.level, 'info');
-                    assert.ok(obj.msg.indexOf('writing to kafka') !== -1);
-
-                    logger.destroy();
-                    client.zk.close();
-                    proc.connection.connection._connection.destroy();
-                    stream.kafka_stop();
-                    stream.client.socket.destroy();
-                    assert.end();
-                });
-
-                logger.info('writing to kafka');
-            });
-        });
-    });
-
-    socket.once('error', function () {
-        assert.ok(true, 'skipping kafka test');
+        server.close();
+        logger.destroy();
         assert.end();
     });
+
+    var logger = Logger({
+        meta: {
+            team: 'rt',
+            project: 'foobar'
+        },
+        backends: {
+            kafka: KafkaBackend({
+                leafHost: 'localhost',
+                leafPort: server.port
+            })
+        }
+    });
+
+    logger.info('writing to kafka');
 });
